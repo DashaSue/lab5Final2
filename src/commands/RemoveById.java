908c81731b85bb6478fe99b0add81888f8a62f89
morisol <anna.ushnova@gmail.com>
2022-03-14 20:54:54 +0300

    remove by id FINAL

--- src/commands/RemoveById.java
+++ src/commands/RemoveById.java
@@ -3,6 +3,7 @@ package commands;
 import data.SpaceMarine;
 import utilities.Decoder;
 
+
 import java.util.LinkedList;
 
 public class RemoveById extends CommandAbstract{
@@ -14,8 +15,19 @@ public class RemoveById extends CommandAbstract{
     public void execute(String[] args, LinkedList<SpaceMarine> list, CommandManager commandManager){
         if (args.length != 1) System.out.println("Неверный id");
         else{
-            try{
-                System.out.println("Элемент с id " + Decoder.list.remove(Integer.parseInt(args[0]) - 1).getId() + " удален");
+            try {
+                boolean exist = false;
+                for (SpaceMarine marine : Decoder.list) {
+                    if (Integer.parseInt(args[0]) == marine.getId()){
+                        System.out.println("Элемент с id " + marine.getId() + " удален");
+                        Decoder.list.remove(marine);
+                        exist = true;
+                        break;
+                    }
+                }
+                if (!exist){
+                    System.out.println("В коллекции нет элемента с указанным id");
+                }
             }catch(Exception e){
                 System.out.println("Коллекция пустая");
             }
